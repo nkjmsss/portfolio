1926f617562fcc37d3c047da71a018c5656ba065
nkjmsss <nakajima-sota018@g.ecc.u-tokyo.ac.jp>
2019-04-11 07:50:07 +0900

    add: beforeEach func

--- src/plugins/routerOption.js
+++ src/plugins/routerOption.js
@@ -1,4 +1,10 @@
 export default ({ app, store }) => {
+  app.router.beforeEach((to, from, next) => {
+    // eslint-disable-next-line
+    console.log(to)
+    next()
+  })
+
   app.router.afterEach((to, from) => {
     store.commit('setMenuOpen', false)
   })

640282a4ac7342f588ff5f0bccd9bb6faace05a1
nkjmsss <nakajima-sota018@g.ecc.u-tokyo.ac.jp>
2019-04-16 23:21:46 +0900

    add: hero header

---
+++ src/components/top/hero/index.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  div.hero
+    .hero__title
+      h1.hero__title__main nkjmsss
+      p.hero__title__sub portfolio
+</template>
+
+<style lang="scss" scoped>
+.hero {
+  &__title {
+    text-align: center;
+    line-height: 1;
+    padding: 4rem 0;
+
+    &__main {
+      font-size: 3.5rem;
+      margin-bottom: 1.5rem;
+    }
+
+    &__sub {
+      font-size: 1.5rem;
+      text-align: inherit;
+    }
+  }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,20 @@
 <template lang="pug">
-  div hoge
+  div
+    v-hero
+    v-h1 about
 </template>
 
 <script>
 // import load from '~/plugins/longLoading'
+import vHero from '~/components/top/hero'
+import vH1 from '~/components/slot/h1'
 
 export default {
   // mixins: [load],
+  components: {
+    vHero,
+    vH1,
+  },
 }
 </script>
 

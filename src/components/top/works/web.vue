455a45e6e62f3c9b69f59ad2293c6673ff239cab
nkjmsss <nakajima-sota018@g.ecc.u-tokyo.ac.jp>
2019-04-22 17:07:40 +0900

    fix: use unique id

--- src/components/top/works/web.vue
+++ src/components/top/works/web.vue
@@ -8,7 +8,9 @@
       viewBox="0 0 263.11 212.37"
     )
       defs
-        clipPath#clip-path
+        clipPath(
+          :id="pathID"
+        )
           path(
             d="M129.09,28.09,7.46,64.93a8.51,8.51,0,0,0-6,8.14V139.3a8.5,8.5,0,0,0,6,8.14l121.63,36.85a8.45,8.45,0,0,0,4.93,0l121.64-36.85a8.51,8.51,0,0,0,6-8.14V73.07a8.52,8.52,0,0,0-6-8.14L134,28.09A8.45,8.45,0,0,0,129.09,28.09Z"
           )
@@ -24,10 +26,17 @@
 <script>
 export default {
   name: 'WebWork',
+
   props: {
     item: {
       type: Object,
       required: true,
+    },
+  },
+
+  computed: {
+    pathID() {
+      return `cp-${this.$uuid}`
     },
   },
 }
